8605b496d6d5c1e9c9726cba7ecfd5a013aca3b1
MyoThet <mr.myothet@gmail.com>
2026-05-04 23:28:37 +0700

    CRUD Operations with EF Core Database-First in ASP.NET Core MVC (jQuery AJAX)

---
+++ DotNetTrainingBatch0.MvcAjax/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DotNetTrainingBatch0.MvcAjax.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
